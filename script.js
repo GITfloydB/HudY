26c9341ad15baca8317cb7f4f8ebb15f5aa2e1aa
GITfloydB <138597405+GITfloydB@users.noreply.github.com>
2025-04-01 03:52:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function playSound() {
     var audioPlayer = document.getElementById("audioPlayer");
-    audioPlayer.src = "HudyInterlude.m4a"; // Change "uploaded_sound.mp3" to the path of the uploaded sound file on your server
+    audioPlayer.src = "nicesthudyinterlude.wav"; // Change "uploaded_sound.mp3" to the path of the uploaded sound file on your server
     audioPlayer.play();
 }
